bf5d61b97a267c04cd51dc87bdefba30a271b3ec
Etudiants <etudiants@cm-bdx-info-24.local>
2026-02-25 13:52:06 +0100

    ArtWork et ArtWorkId Models

--- ArtGuessr/ArtGuessr/Models/ArtWork.swift
+++ ArtGuessr/ArtGuessr/Models/ArtWork.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct ArtWork: Identifiable, Codable {
     let id: Int
     let image: URL
@@ -11,5 +13,28 @@ struct ArtWork: Identifiable, Codable {
         case name = "title"
         case year = "objectBeginDate"
         case artist = "artistDisplayName"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        
+        // On récupère l'URL de l'image (l'API renvoie une String)
+        let imageString = try container.decode(String.self, forKey: .image)
+        self.image = URL(string: imageString) ?? URL(string: "https://via.placeholder.com/150")!
+        
+        self.name = try container.decode(String.self, forKey: .name)
+        self.year = try container.decode(Int.self, forKey: .year)
+        self.artist = try container.decode(String.self, forKey: .artist)
+    }
+    
+    // Pour satisfaire Encodable (si tu as besoin d'exporter l'objet en JSON)
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(image.absoluteString, forKey: .image)
+        try container.encode(name, forKey: .name)
+        try container.encode(year, forKey: .year)
+        try container.encode(artist, forKey: .artist)
     }
 }
