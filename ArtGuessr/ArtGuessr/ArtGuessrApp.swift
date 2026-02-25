a6fcd99ae556ac419dd19edbe7504db9f98d95cf
Theo Dumontet <144424403+Terapyy18@users.noreply.github.com>
2026-02-25 11:29:43 +0100

    ArtGuessr Initialisation

---
+++ ArtGuessr/ArtGuessr/ArtGuessrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGuessrApp.swift
+//  ArtGuessr
+//
+//  Created by Etudiants on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArtGuessrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtGuessr/ArtGuessrTests/ArtGuessrTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGuessrTests.swift
+//  ArtGuessrTests
+//
+//  Created by Etudiants on 25/02/2026.
+//
+
+import Testing
+@testable import ArtGuessr
+
+struct ArtGuessrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
